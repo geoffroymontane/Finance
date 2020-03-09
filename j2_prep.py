106b45c46b3d8af7f328b911f0fbd5166a851bc1
diogobodas <diogobodas@gmail.com>
2020-03-09 17:01:06 +0100

    j2_prep

---
+++ j2_prep.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+
+def cov_N():
+
+	N = 0
+
+	data = pd.DataFrame()
+	path = "data_bourse/"
+
+	for filename in os.listdir(path):
+		temp = pd.read_csv(path + filename)
+		data[filename[:3]] = (temp[filename[:3] + "~high"] + temp[filename[:3] + "~low"])/2
+
+	return data.cov,N
+       
